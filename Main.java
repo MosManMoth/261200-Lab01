5239060beb23311480a4d09be4115f50c81c7936
MosManMoth <matha828@gmail.com>
2025-06-30 10:14:40 +0700

    First Submition

---
+++ Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+                Turtle bob = new Turtle();
+                bob.penColor("green");
+                bob.width(5.0);
+                bob.speed(2.0);
+
+                //for(int i=0; i < 3; i++){
+                   // bob.forward(100);
+                    //bob.left(120);
+                //}
+
+                for(int i=0; i < 5; i++){
+                    bob.forward(200);
+                    bob.left(145);
+                }
+        }
+    }
